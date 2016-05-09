2e9b4ca65d0568838eab63b74aab90002b7ab559
chenliang <chenliang@haizhi.com>
2016-05-09 16:23:11 +0800

    update getpass

--- s13-day1/getpass.py
+++ s13-day1/getpass.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 # Author: ChenLiang
 
-#import getpass
-#
-#username = input("请输入用户名: ")
-#password = getpass.getpass("请输入你的密码: ")
+import getpass
 
-#print(username, password)
+username = input("请输入用户名: ")
+password = getpass.getpass("请输入你的密码: ")
+
+print(username, password)
