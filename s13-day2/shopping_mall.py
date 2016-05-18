c306ec2826a148764fd8ebe3450ba9530e7673bd
chenliang <chenliang@haizhi.com>
2016-05-18 21:50:37 +0800

    update

--- s13-day2/shopping_mall.py
+++ s13-day2/shopping_mall.py
@@ -2,18 +2,18 @@
 # -*- coding: utf-8 -*-
 # Author: ChenLiang
 
-salary = input("请输入您的工资金额: ")
-if salary.isdigit():
-    salary = int(salary)
+salary = input("请输入您的工资金额: ")  # 定义salary变量,用于后续判断
+if salary.isdigit():  # 判断输入内容是否为数字
+    salary = int(salary)  # 转换为整形
 else:
     exit("无效的数据类型,谢谢.")
 
-welcome_msg = '欢迎来到新世纪购物中心'.center(50, '-')
+welcome_msg = '欢迎来到新世纪购物中心'.center(50, '-')  # 定义环境信息
 
 print(welcome_msg)
 
-exit_flag = False
-product_list = [
+exit_flag = False  # 定义退出标志位,用于结束循环使用
+product_list = [  # 定义商品列表
     ('Iphone', 5888),
     ('Mac Air', 8000),
     ('Mac Pro', 9000),
@@ -24,34 +24,33 @@ product_list = [
     ('Cloth', 200),
 ]
 
-shop_car = []
+shop_car = []  # 定义购物车变量
 
-while not exit_flag:
+while not exit_flag:  # 当exit_flag标志位不为True,执行后面的代码
     # while exit_flag is not True:
     # for product_item in product_list:
     #    p_name,p_price = product_item
     # enumerate 枚举函数,打印下标
-    print("商品列表".center(55, "-"))
-    for item in enumerate(product_list):
+    print("商品列表".center(55, "-"))  # 格式化输出
+    for item in enumerate(product_list):  # 循环枚举商品列表,打印商品编号及商品信息
         index = item[0]
         p_name = item[1][0]
         p_price = item[1][1]
         print(index, ':', p_name, p_price)
 
-    user_choice = input("[q=quit,c=check]您输入您需要购买的产品编号?: ")
-    if user_choice.isdigit():  # 肯定是选择商品
-        user_choice = int(user_choice)
-        if user_choice < len(product_list):
-            p_item = product_list[user_choice]
-            if p_item[1] <= salary:  # 可以购买
+    user_choice = input("[q=quit,c=check]您输入您需要购买的产品编号?: ")  # 定义用户输入变量
+    if user_choice.isdigit():  # 肯定是选择商品,判断是否输入为数字
+        user_choice = int(user_choice)  # 将用户的输入转换为整形
+        if user_choice < len(product_list):  # 判断用户选择是否超出商品列表长度
+            p_item = product_list[user_choice]  # 获取用户选择的商品
+            if p_item[1] <= salary:  # 判断商品金额是否小于或者等于工资金额,小于则可以购买,否则提示余额不足
                 shop_car.append(p_item)  # 放入购物车
                 salary -= p_item[1]  # 扣钱
-                print("已添加商品 %s 到购物车, 您当前余额为, \033[31;1m%s\033[0m" %
-                      (list(p_item), salary))
+                print("已添加商品 %s 到购物车, 您当前余额为, \033[31;1m%s\033[0m" % (list(p_item), salary))  # 打印购买商品信息,同时显示余额信息
             else:
-                print("您的余额不足以购买此产品: \033[32;1m%s\033[0m 当前余额: \033[32;1m%s\033[0m" % p_item, salary)
+                print("您的余额不足以购买此产品: \033[32;1m%s\033[0m" % salary)  # 余额不足提示
     else:
-        if user_choice == 'q' or user_choice == 'quit':
+        if user_choice == 'q' or user_choice == 'quit':  # 判断用户选择q或者quit,就退出系统,同时打印用户购买商品列表
             print("您所购买的产品信息如下".center(50, "*"))
             for item in shop_car:
                 print(list(item))
@@ -59,7 +58,7 @@ while not exit_flag:
             print("您的余额为: \033[32;1m%s\033[0m" % salary)
             print("Bye")
             exit_flag = True
-        elif user_choice == 'c' or user_choice == 'check':
+        elif user_choice == 'c' or user_choice == 'check':  # 判断用户选择c或者check,打印用户购买商品列表,同时提示余额信息
             print("您所购买的产品信息如下".center(50, "*"))
             for item in shop_car:
                 print(list(item))
